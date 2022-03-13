02d12b9ed3ab2204ec10f5a13f901ee238a35d54
Weixiong Zheng <wzheng@carbon3d.com>
2022-03-12 19:27:26 -0800

    insertion sort iterator version

--- lecture3/insertion_sort.cc
+++ lecture3/insertion_sort.cc
@@ -7,7 +7,7 @@
 // TODO: extend the sort to generic types and compare
 // The following function sort vector in ascending order by default
 template <typename T, typename Compare = std::greater<T>>
-void InsertionSort(std::vector<T>* v) {
+void InsertionSortOrg(std::vector<T>* v) {
   for (int i = 1; i < v->size(); ++i) {
     int k = i;
     while (k > 0 && Compare()((*v)[k-1], (*v)[k])) {
@@ -17,10 +17,25 @@ void InsertionSort(std::vector<T>* v) {
   }
 }
 
+// Insertion sort using iterators instead of index
+template <typename T, typename Compare = std::greater<T>>
+void InsertionSort(std::vector<T>* v) {
+  for (typename std::vector<T>::iterator itr = std::next(v->begin()); itr != v->end(); ++itr) {
+    typename std::vector<T>::iterator key = itr;
+    typename std::vector<T>::iterator key_prev = std::prev(key);
+    while (key != v->begin() && Compare()(*key_prev, *key)) {
+      // Note that iter_swap swaps the element value not really the iterator
+      std::iter_swap(key, key_prev);
+      --key;
+      key_prev = std::prev(key);
+    }
+  }
+}
+
 int main() {
   std::vector<int> in = {5, 2, 4, 6, 1, 3};
   std::cout << in << std::endl;
-  InsertionSort(&in);
+  InsertionSortOrg(&in);
   std::cout << in << std::endl;
   InsertionSort<int, std::less<int>>(&in);
   std::cout << in << std::endl;

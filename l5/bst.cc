24b1c95202d4c691dcd9d01692d1442cffb59bd8
victor <zwxne2010@gmail.com>
2022-03-20 02:44:33 -0700

    rank is not yet correct

--- l5/bst.cc
+++ l5/bst.cc
@@ -25,7 +25,7 @@ class BST {
 public:
   BST() = default;
   void Insert(const DataType& e);
-  DataType Rank(const DataType& t) const { return 0; };
+  int Rank(const DataType& t) const;
 
   void InorderPrint() const;
   void InorderPrint(const NodePtr& node, std::ostream* s) const;
@@ -34,6 +34,7 @@ private:
   NodePtr root_ = nullptr;
 
   void Insert(NodePtr* root, NodePtr* node, const NodePtr& p);
+  std::shared_ptr<Node<DataType>> FindMinMax(const NodePtr& root, const DataType& e) const;
 };
 
 template <typename DataType>
@@ -49,7 +50,7 @@ void BST<DataType>::InorderPrint(const NodePtr& node, std::ostream* oss) const {
   if (node->left) {
     InorderPrint(node->left, oss);
   }
-  *oss << node->value << ',';
+  *oss << node->value << '(' << node->size << "),";
   if (node->right) {
     InorderPrint(node->right, oss);
   }
@@ -82,9 +83,27 @@ void BST<DataType>::Insert(NodePtr* root, NodePtr* node, const NodePtr& p) {
   Insert(&((*root)->right), node, *root);
 }
 
+template <typename DataType>
+std::shared_ptr<Node<DataType>> BST<DataType>::FindMinMax(const NodePtr& root, const DataType& e) const {
+  if (!root) return nullptr;
+  if (e <= root->value) return FindMinMax(root->right, e);
+  if (!root->left) return root;
+  return FindMinMax(root->left, e);
+}
+
+template <typename DataType>
+int BST<DataType>::Rank(const DataType& e) const {
+  NodePtr node = FindMinMax(root_, e);
+  if (!node) return root_->size;
+  return root_->size - node->size;
+}
+
 int main() {
   BST<int> bst;
   for (int e : {8, 4, 10, 14, 3, 9, 16, 2, 7, 1}) bst.Insert(e);
   bst.InorderPrint();
+  std::cout << "Rank of 17: " << bst.Rank(17) << std::endl;
+  std::cout << "Rank of 16: " << bst.Rank(16) << std::endl;
+  std::cout << "Rank of 15: " << bst.Rank(15) << std::endl;
   return 0;
 }
